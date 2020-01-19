4496a0bb26e827292cd8552c6fd208581d86a161
deusimardamiao <deusimar@ufpi.edu.br>
2020-01-19 10:34:52 -0300

    back-end (ok)

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -2,6 +2,7 @@ const axios = require('axios');
 
 const Dev = require('./../models/Dev');
 const parseStringAsArray = require('./../utils/parseStringAsArray');
+const { findConnections, sendMessage } = require('./../websocket');
 
 module.exports = {
     async index(req, res) {
@@ -35,6 +36,13 @@ module.exports = {
                 techs: techsArray,
                 location
             });
+
+            const sendSocketMessageTo = findConnections(
+                { latitude, longitude },
+                techsArray
+            );
+
+            sendMessage(sendSocketMessageTo, 'new-dev', dev);
         }
 
         res.json(dev);
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const http = require('http');
 require('dotenv').config();
 
 const routes = require('./routes');
+const { setupWebsocket } = require('./websocket');
 
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
@@ -17,4 +22,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(process.env.PORT);
+server.listen(process.env.PORT);
